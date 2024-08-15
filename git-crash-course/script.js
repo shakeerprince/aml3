b033f14464194669eb3580bb518076035dd34a8b
shaker <hydrashaker@gmail.com>
2024-08-15 16:08:42 +0530

    replace cncat with lit

--- git-crash-course/script.js
+++ git-crash-course/script.js
@@ -1,5 +1,13 @@
-let js = 'script';
-console.log('hi');
-console.log('shaker');
+let js = "script";
 
+console.log("shaker");
 
+function isAbove18() {
+  return age > 18;
+}
+console.log("isAbove18");
+
+let nam = "Shaker";
+function log() {
+  console.log(`My Name is + ${nam}`);
+}
